b835c2cb073653e4f4631226c5d53b4d2e77890a
Andrew Wilborn <andrew.j.wilborn@sbcglobal.net>
2023-07-18 10:44:56 -0400

    ADDED authcontext

---
+++ context/AuthContext.jsx
@@ -0,0 +1,29 @@
+"use client"
+
+import { createContext, useState } from "react"
+
+export const AuthContext = createContext()
+
+export function AuthProvider({ childern }){
+  const [user, setUser] = useState()
+  const [token, setToken] = useState()
+
+  const handleLogin = (data) => {
+    setUser(data.user)
+    setToken(data.token)
+    sessionStorage.setItem("user", JSON.stringify(data.user))
+    sessionStorage.setItem("token", JSON.stringify(data.token))
+  }
+
+  const handleLogout = () => {
+    setUser()
+    setToken()
+    sessionStorage.clear()
+  }
+
+  return 
+  <AuthContext.Provider value={{ user, token, handleLogin, handleLogout }}>
+    {childern}
+  </AuthContext.Provider>
+
+}
